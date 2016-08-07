d1e883ec575c82812fed7d34895fafeb7b365e92
Joseph Tracy <github.com@revisualized.com>
2016-08-06 20:34:03 -0700

    Create Convert_Celsius_to_Fahrenheit.js

---
+++ Convert_Celsius_to_Fahrenheit.js
@@ -0,0 +1,28 @@
+const convertToF = c => c * 9/5 + 32;
+
+/*
+function convertToF(celsius) {
+  return celsius * 9/5 + 32;
+}
+
+// Change the inputs below to test your code
+convertToF(30);
+*/
+
+/*
+function convert(Tc) {
+  // Only change code below this line
+  Tf = Tc * 9/5 + 32;
+
+  // Only change code above this line
+  if(typeof Tf !== 'undefined') {
+  return Tf;
+  } else {
+    return "Tf not defined";
+  }
+     // return (celsius) ? celsius * 9/5 + 32 : "fahrenheit not defined";
+}
+
+// Change the inputs below to test your code
+convert(30);
+*/
