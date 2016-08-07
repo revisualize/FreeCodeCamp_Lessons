1b3f8ae1ae62937ac528abeab8f7f4ffb00b74ad
Joseph Tracy <github.com@revisualized.com>
2016-08-06 19:58:13 -0700

    Create Factorialize_a_Number.js

---
+++ Factorialize_a_Number.js
@@ -0,0 +1,31 @@
+const factorialize = (num) => (num === 0) ? 1 : num * factorialize(num -1);
+/*
+function factorialize(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorialize(num -1);
+}
+*/
+
+/* --- This is old code that I refactored to use recursion:
+  for (var factorial = 1, i = 2; i <= num; i++) {
+    factorial = factorial * i;
+  }
+  return factorial;
+
+   --- This is older code that I refactored to what is above
+  if (num === 0) {
+    return 1;
+  }
+  var fib = 1;
+  for (var i = 2; i <= num; i++) {
+    fib = fib * i;
+  }
+  return fib;
+*/
+
+factorialize(5); // 120
+factorialize(10); // 3628800
+factorialize(20); // 2432902008176640000
+factorialize(0); // 1 ... I'm not sure why that is. Shouldn't it be 0?
