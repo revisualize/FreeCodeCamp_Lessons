637d8ab96aa1a849b57af881ff3da7bb3d70260a
Joseph Tracy <github.com@revisualized.com>
2016-08-09 08:01:03 -0700

    Update Roman_Numeral_Converter-Refactor.js

--- Roman_Numeral_Converter-Refactor.js
+++ Roman_Numeral_Converter-Refactor.js
@@ -1,5 +1,4 @@
-
-const romans = {
+const numerals = {
          1000: 'M', 
          900: 'CM', 
          500: 'D', 
@@ -13,4 +12,29 @@ const romans = {
          5: 'V', 
          4: 'IV', 
          1: 'I'
+    }
+    
+const keys = Object.keys(numerals).sort((a, b) => b - a).map(x => Number(x));
+//  keys.map(x => numerals[x]);
+
+function convertToRoman(num) {
+	function conversion(num, romans) {
+		return (num > keys[i]) ? conversion(num - i, romans + numerals[i]) : "";
+	}
+	return conversion(num, "")
+}
+
+convertToRoman(1)
+
+
+
+
+function conversion(num, romans) {
+        if (num===0)
+              return "";
+        for(var i=0; i<keys.length; i++) {
+              var div = Math.floor(num/keys[i]);
+              if (div >0 && num%keys[i]===0)
+                  return conversion(num - div *keys[i], romans + numerals[i].repeat(div));
+        }
     }
