03d391573fa7bef1d240e2b095c5167a93be5c1d
Joseph <github.com@revisualized.com>
2016-09-06 19:02:30 -0700

    Update Sum_All_Odd_Fibonacci_Numbers.js
    Refactor the code to not use .filter() or .reduce() and improve efficiency. :)

--- Sum_All_Odd_Fibonacci_Numbers.js
+++ Sum_All_Odd_Fibonacci_Numbers.js
@@ -2,6 +2,21 @@ function sumFibs(num) {
   var accu = 0;
   var prev = 1;
   var curr = 1;
+  var fibOdd = 1;
+  while (curr <= num) {
+     if (curr % 2 === 1) { fibOdd += curr; }
+     accu = prev + curr;
+     prev = curr;
+     curr = accu;  
+  }
+  return fibOdd;
+}
+
+/* Code from 2016 Sept 05:
+function sumFibs(num) {
+  var accu = 0;
+  var prev = 1;
+  var curr = 1;
   var fibOdd = [1];
   while (curr <= num) {
      fibOdd.push(curr);
@@ -11,7 +26,7 @@ function sumFibs(num) {
   }
   return fibOdd.filter(x => x % 2 === 1).reduce((pre, cur) => pre + cur);
 }
-
+*/
 sumFibs(1); // should return a number.
 sumFibs(1000); // should return 1785.
 sumFibs(4000000); // should return 4613732.
