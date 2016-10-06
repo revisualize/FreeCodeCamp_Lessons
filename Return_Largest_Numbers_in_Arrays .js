aaa57693979fe8d1cf1df43131611c8b70b5e613
Joseph <github.com@revisualized.com>
2016-10-06 15:58:08 -0700

    Update Return_Largest_Numbers_in_Arrays .js

--- Return_Largest_Numbers_in_Arrays .js
+++ Return_Largest_Numbers_in_Arrays .js
@@ -4,7 +4,13 @@ const largestOfFour = arr => arr.map(x => Math.max(...x));
 function largestOfFour(arr) {
  return arr.map(x => Math.max(...x));
 }
+
+Without using ES6
+
+function largestOfFour(arr) { return arr.map(function (x) { return Math.max.apply(null, x); }); }
+
 */
+
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]); // should return [27,5,39,1001].
 largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]); // should return [9, 35, 97, 1000000].
