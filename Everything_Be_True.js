cd3a79f4e50cbcc148f68bd54347e66f9c022526
Joseph <github.com@revisualized.com>
2016-11-04 21:53:05 -0700

    Create Everything_Be_True.js

---
+++ Everything_Be_True.js
@@ -0,0 +1,13 @@
+function truthCheck(collection, pre) {
+  return collection.every(x => x.hasOwnProperty(pre) && Boolean(x[pre]));
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"); // should return true.
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"); // should return false.
+truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age"); // should return false.
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat"); // should return false
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat"); // should return true
+truthCheck([{"single": "yes"}], "single"); // should return true
+truthCheck([{"single": ""}, {"single": "double"}], "single"); // should return false
+truthCheck([{"single": "double"}, {"single": undefined}], "single"); // should return false
+truthCheck([{"single": "double"}, {"single": NaN}], "single"); // should return false
