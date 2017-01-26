4f2b126f1724fb728fafc5b553cb3ea142094b87
Joseph <github.com@revisualized.com>
2017-01-25 19:21:13 -0800

    Create Arguments_Optional.js

---
+++ Arguments_Optional.js
@@ -0,0 +1,22 @@
+function addTogether() {
+  var x = arguments[0];
+  if (typeof x !== "number") return undefined;
+  if (arguments[1] !== undefined) {
+    var y = arguments[1];
+    if (typeof y !== "number") {
+      return undefined;
+    } else {
+      return x+y;
+    }
+  }
+  return function(y) {
+    if (typeof y !== "number") return undefined;
+    return x + y;
+  };
+}
+
+addTogether(2, 3); // should return 5.
+addTogether(2)(3); // should return 5.
+addTogether("http://bit.ly/IqT6zt"); // should return undefined.
+addTogether(2, "3"); // should return undefined.
+addTogether(2)([3]); // should return undefined.
