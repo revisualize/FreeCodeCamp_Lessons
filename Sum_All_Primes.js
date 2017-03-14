bf043f0e945ea48661d86c3bddfb7e446f58a3ae
Joseph <github.com@revisualized.com>
2017-03-13 21:59:23 -0700

    Update Sum_All_Primes.js

--- Sum_All_Primes.js
+++ Sum_All_Primes.js
@@ -1,6 +1,37 @@
 // My solution for https://www.freecodecamp.com/challenges/sum-all-primes
 
 function sumPrimes(num) {
+  if (num<=1) return 0;
+  if (num===2) return 2;
+  var primes = [2];
+  var total = 2;
+  for (var i=3; i <= num; i += 2) {
+    if (isPrime(i)) {
+      primes.push(i);
+      total += i;
+    }
+  } 
+  return total;
+  
+  function isPrime(p) {
+    var sqrt = Math.ceil(Math.sqrt(p));
+    for (var j=0; j < primes.length && primes[j] <= sqrt; j++) {
+      if (p % primes[j]===0) return false;
+    }
+    return true;
+  }
+}
+
+console.time();
+sumPrimes(500000);
+console.timeEnd();
+// default: 40.899ms
+
+
+
+/* My first solution is below:
+
+function sumPrimes(num) {
    var primes = [2,3,5,7];
    var total = 17;
    function isPrime(num) {
@@ -25,4 +56,9 @@ function sumPrimes(num) {
   return total;
 }
 
-sumPrimes(100);
+console.time();
+sumPrimes(500000);
+console.timeEnd();
+
+// default: 3491.986ms
+
