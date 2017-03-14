5129a98088f4938c764a6868a31af411a03693e0
Joseph <github.com@revisualized.com>
2017-03-13 22:34:29 -0700

    Update Sum_All_Primes.js

--- Sum_All_Primes.js
+++ Sum_All_Primes.js
@@ -62,3 +62,57 @@ console.timeEnd();
 
 // default: 3491.986ms
 
+*/
+
+
+
+
+
+
+/*
+
+I'm putting this down here. 
+I just want to keep it as a personal record
+of some of the silly stuff I tried that didn't work.
+
+var maxCalculated = 8, primes = [[2,2] , [3,5] , [5,10] , [7,17]];
+
+function sumPrimes(num) {
+   if (num > maxCalculated) {
+      maxCalculated = num; 
+   } else {
+      let index = primes.findIndex(function (el, index, array) { return el[0] > num; }); 
+      return primes[index-1][1];
+   }
+}
+function isPrime(num) {
+   if (num < 2 || num % 2 === 0 || num % 3 === 0 || num % 5 === 0 || num % 7 === 0) {
+      return false;
+   }
+   for (var i = 4; i < primes.length; i++) {
+        if (primes[i][0] % num === 0) {
+           return false;
+        }
+   }
+   return true;
+}
+
+sumPrimes(10);
+
+
+var primes = [];
+
+function sumPrimes(num) {
+   if (num > primes[primes.length-1]) {
+     return "If true stuff";
+   } else {
+      var index = primes.findIndex(function (el, index, array) { 
+        return el > num; 
+        }); 
+      return primes[index-1];
+   }
+}
+
+sumPrimes(7);
+
+*/
