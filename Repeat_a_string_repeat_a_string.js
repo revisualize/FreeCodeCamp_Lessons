e9870777ef11003654a5bb6e094907da73b4600e
Joseph <github.com@revisualized.com>
2017-04-05 19:06:26 -0700

    Update Repeat_a_string_repeat_a_string.js

--- Repeat_a_string_repeat_a_string.js
+++ Repeat_a_string_repeat_a_string.js
@@ -4,15 +4,38 @@ Note: I do know about the ES6 String.prototype.repeat()
 I didn't want to use it. I wanted to stay with the nature of the challenge.
 Also, using negative numbers results in an error with .repeat()
 */
+/*
+function repeatStringNumTimes(str, num) {
+    if (num <= 0) return "";
+    var result = "";
+    for (var i = 0; i <num; i++) {
+        result += str;
+    }
+    return result;
+}
+*/
+/*
+function repeatStringNumTimes(str, num) {
+     if (num <= 0) return "";
+     return str.repeat(num);
+}
+*/
+/*
+function repeatStringNumTimes(str, num) {
+     if (num <= 0) return "";
+     var result = new Array(num).fill(str);
+     return result.join('');
+}
+*/
+
+
 /* Testing for my ES6 refactor of the old code below.
 function repeatStringNumTimes(str, num) {
   // repeat after me
   if (num > 0) {
-		return str += repeatStringNumTimes(str, num-1);
-	}
-	else {
-		return "";
-	}
+      return str += repeatStringNumTimes(str, num-1);
+  }
+  return "";
 }
 */ 
 /* Old code
