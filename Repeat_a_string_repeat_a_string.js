1fb5d4a1b9f9e0ac93ae51d2a0d5b1b167f9d5be
Joseph <github.com@revisualized.com>
2017-08-16 21:08:54 -0700

    Update Repeat_a_string_repeat_a_string.js

--- Repeat_a_string_repeat_a_string.js
+++ Repeat_a_string_repeat_a_string.js
@@ -1,5 +1,12 @@
 const repeatStringNumTimes = (str, num) => num > 0 && num != Infinity ? str += repeatStringNumTimes(str, num-1) : "";
 /*
+I was playing around with a less complex solution and I came up with this:
+
+const repeatStringNumTimes = (str, num) => (num > 0) ? new Array(num).fill(str).join('') : "";
+
+I actually like this solution. And it's fairly quick.
+*/
+/*
 Note: I do know about the ES6 String.prototype.repeat() 
 I didn't want to use it. I wanted to stay with the nature of the challenge.
 Also, using negative numbers results in an error with .repeat()
