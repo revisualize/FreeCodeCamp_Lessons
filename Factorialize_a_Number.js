7953fcb443c4fa804ce8375aa881d33d5eca2408
Joseph <github.com@revisualized.com>
2018-01-04 19:40:54 -0800

    Update Factorialize_a_Number.js

--- Factorialize_a_Number.js
+++ Factorialize_a_Number.js
@@ -1,21 +1,21 @@
-// This is a better implimentation:
-// This implimentation works with 0, -0, pos and neg numbers.
-function factorial (num) {
-    if (typeof num != 'number' || num != Infinity || num != -Infinity) {
-      return NaN; // Error catching
+function factorial (num, accumulator = 1) {
+    if (typeof num != 'number' ||
+               num == Infinity || 
+               num == -Infinity ||
+               num % 1 !== 0) {
+        return NaN;
     }
-    let sign = Math.sign(num);
-    if (sign === -0) {
-      return -1;
+    if (num === 0 || num === 1 || num === -1) {
+        return accumulator;
     }
-    if (sign <= 0) {
-      num *= -1;
+    if (num < 0) {
+        return factorial(num+1, accumulator*num);
     }
-    if (num === 0) {
-        return 1;
+    if (num > 0) {
+        return factorial(num-1, accumulator*num);
     }
-    return sign * num * factorial(num - 1);
 }
+
 
 
 // const factorialize = (num) => (num === 0) ? 1 : num * factorialize(num -1);
@@ -49,4 +49,4 @@ function factorialize(num) {
 factorialize(5); // 120
 factorialize(10); // 3628800
 factorialize(20); // 2432902008176640000
-factorialize(0); // 1 ... I'm not sure why that is. Shouldn't it be 0?
+factorialize(0); // 1
